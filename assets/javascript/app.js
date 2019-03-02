6be0cd62ff07dd08a4d2e733a34885d61f8b5a31
ksd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-03-02 01:33:13 -0600

    Submit button now sucessfully creates new buttons. Made color adjustments to the page.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,31 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-var topics = ["excitement", "fear", "boredom", "disgust", "rage", "love", "annoyance", "gratitude", "amusement", "loneliness"];
+    // array that holds topics for the buttons and gifs
+    var topics = ["excitement", "fear", "boredom", "disgust", "rage", "love", "annoyance", "gratitude", "amusement", "loneliness"];
 
-for (var i = 0; i < topics.length; i++) {
-    // loop that appends a button for each string in the array
-    var topicButton = $("<button>");
-    topicButton.attr("id", topics[i] + "Button");
-    topicButton.text(topics[i]);
-    topicButton.addClass("btn btn-warning");
-    // topicButton.addClass("btn btn-warning");
-    $(".buttonSpace").append(topicButton);
-};
+    function createButtons() {
+        // delete buttons prior to adding new buttons to avoid repeat buttons
+        $(".buttonSpace").empty();
+        // for loop that appends a button for each string in the array
+        for (var i = 0; i < topics.length; i++) {
+            var topicButton = $("<button>");
+            topicButton.attr("id", topics[i] + "Button");
+            topicButton.addClass("emotion btn btn-warning");
+            topicButton.attr("data-name", topics[i]);
+            topicButton.text(topics[i]);
+            $(".buttonSpace").append(topicButton);
+        };
+    }
 
+    $("#submitButton").click(function (event) {
+        // stop button from trying to submit form
+        event.preventDefault();
+        var inputVal = $("#searchBox").val().trim();
+        // push input into the array
+        topics.push(inputVal);
+        createButtons();
+        // clear out search box
+        $("#searchBox").val("");
+    });
+    createButtons()
 });
